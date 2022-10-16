dd95bf9830b9fa68787d309e8402b6f15d9c522a
wkdewey <william.dewey@unl.edu>
2022-10-16 11:26:15 -0500

    add overrides to post either figures or monasteries

---
+++ scripts/overrides/file_csv.rb
@@ -0,0 +1,45 @@
+class FileCsv
+  def transform_es
+      # Calling `super` here uses Datura's FileType.transform_es rather
+      # than its FileCsv.transform_es, so copying latter's code for now
+      puts "transforming #{self.filename}"
+      es_doc = []
+      table = table_type
+      @csv.each do |row|
+        if !row.header_row?
+          es_doc << row_to_es(@csv.headers, row, table)
+        end
+      end
+      if @options["output"]
+        filepath = "#{@out_es}/#{self.filename(false)}.json"
+        File.open(filepath, "w") { |f| f.write(pretty_json(es_doc)) }
+      end
+      es_doc
+  end
+
+  def read_csv(file_location, encoding="utf-8")
+      CSV.read(file_location, {
+        encoding: encoding,
+        headers: true
+      })
+  end
+
+  def row_to_es(headers, row, table)
+    # process the cases and people tables with different overrides
+    puts "processing " + row["id"]
+    if table == "figures"
+      CsvToEs.new(row, options, @csv, self.filename(false)).json
+    elsif table == "monasteries"
+      CsvToEsMonasteries.new(row, options, @csv, self.filename(false)).json
+    end
+  end
+
+  def table_type
+    if self.filename.include? "figures"
+      "figures"
+    else
+      "monasteries"
+    end
+  end
+
+end
